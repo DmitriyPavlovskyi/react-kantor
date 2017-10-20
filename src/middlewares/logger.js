bba80997ca1b6e567c8229c8726c9f7f6d1403cd
Dmytro Pavlovskyi1 <pavlovskyidmitriy@gmail.com>
2017-10-20 17:15:03 +0300

    middleware logger added

---
+++ src/middlewares/logger.js
@@ -0,0 +1,11 @@
+export default store => next => action => {
+  // Состояние до и после того как задиспатчится экшн
+  console.log('middleware logger state before',store.getState());
+  next(action);
+  console.log('middleware logger state after',store.getState());
+}
+
+// Middlewares нужны для обработки side effects,
+// и это пожалуй единственное место, где мы можем лезть в store
+// Они будут вызываться на каждый экшн!
+// Именно по этому их надо писать максимально реюзабельными!
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from '../reducer';
+import logger from '../middlewares/logger';
 
-const store = createStore(reducer);
+// Сюда записываем все мидлвары которые будут через запятую
+const enhancer = applyMiddleware(logger);
+
+// А сюда третим параметром их передаем
+const store = createStore(reducer, {}, enhancer);
 
 // Для удобства использования при разработке
 // dev only
