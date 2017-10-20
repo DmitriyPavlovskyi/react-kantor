40fffa3ee10479982649791f08e11727344bec8e
Dmytro Pavlovskyi1 <pavlovskyidmitriy@gmail.com>
2017-10-20 12:06:11 +0300

    Data splited, comment with connect and reducer added

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
 function Comment({comment}) {
   return (
@@ -10,10 +11,19 @@ function Comment({comment}) {
 }
 
 Comment.propTypes = {
+  id: PropTypes.string.isRequired,
+  //from connect
   comment: PropTypes.shape({
     text: PropTypes.string.isRequired,
     user: PropTypes.string.isRequired
   }).isRequired
 };
 
-export default Comment;
+// У connect state отвечает за то, что пришло из store, но так же есть второй аргумент,
+// в котором можно посмотреть что реально пришло в props компоненту
+export default connect((state, ownProps) => {
+  // Ищем в сторе по айдишнику наш коммент и отдаем его в компонент, который его и отрисует
+  return {
+    comment: state.comments.find(comment => comment.id === ownProps.id)
+  };
+})(Comment);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,10 +1,11 @@
 import {combineReducers} from 'redux';
 import counterReducer from './counter';
 import articles from './articles';
+import comments from './comments';
 import filters from './filters';
 
 // ES6 на примере articles, можно передавать только его, без ключа
 export default combineReducers({
   count: counterReducer,
-  articles, filters
+  articles, comments, filters
 });
