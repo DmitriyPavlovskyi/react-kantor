4ae9b5b8d89e87f1e590cd224cf6a6237c670c21
Dmytro Pavlovskyi1 <pavlovskyidmitriy@gmail.com>
2017-11-01 10:33:02 +0200

    NotFound page with routes added

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import Articles from './routes/Articles';
 import UserForm from './UserForm';
 import Filters from './Filters';
+import NotFound from './routes/NotFound';
 import Counter from './Counter';
-import {HashRouter as Router, Route, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom';
+// switch нужен для того чтоб избежать поведения, когда рендерится несколько роутов одновременно
+// Сверху описывать наиболее точные пути
 import 'react-select/dist/react-select.css';
 
 class App extends Component {
@@ -22,9 +25,12 @@ class App extends Component {
               <div><NavLink activeStyle = {{color: 'red'}} to = '/articles'>Articles</NavLink></div>
             </div>
             <UserForm />
-            <Route path = '/counter' component = {Counter} />
-            <Route path = '/filters' component = {Filters} />
-            <Route path = '/articles' component = {Articles} />
+            <Switch>
+              <Route path = '/counter' component = {Counter} />
+              <Route path = '/filters' component = {Filters} />
+              <Route path = '/articles' component = {Articles} />
+              <Route path = '*' component = {NotFound} />
+            </Switch>
           </div>
         </Router>
       );
---
+++ src/components/routes/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NotFound(props) {
+  return (
+    <h1>Page not found</h1>
+  );
+}
+
+NotFound.propTypes = {
+
+};
+
+export default NotFound;
